d8ca2884536b4dc56cfe59cf536dcf0f18ad3648
Gordon Phoon <11506285+cablooshe@users.noreply.github.com>
2026-01-31 17:39:18 -0800

    updated open logic working

--- scripts/MainScene.gd
+++ scripts/MainScene.gd
@@ -19,48 +19,7 @@ func _ready():
 	if act1:
 		render_act(act1)
 	
-	# Setup interaction logic
-	var interaction_area = $"Script Desk/InteractionArea"
-	if interaction_area:
-		interaction_area.body_entered.connect(_on_interaction_area_body_entered)
-		interaction_area.body_exited.connect(_on_interaction_area_body_exited)
 	
-	# Connect to players
-	# Note: In the scene they are named "Actor 1" and "Actor 2"
-	var actor1 = $"Actor 1"
-	var actor2 = $"Actor 2"
-	
-	if actor1:
-		actor1.interact_pressed.connect(func(): _on_player_interact(actor1))
-	if actor2:
-		actor2.interact_pressed.connect(func(): _on_player_interact(actor2))
-
-func _on_interaction_area_body_entered(body):
-	if body is CharacterBody2D and "player_id" in body: # Simple check for Player
-		if not body in players_in_zone:
-			players_in_zone.append(body)
-
-func _on_interaction_area_body_exited(body):
-	if body in players_in_zone:
-		players_in_zone.erase(body)
-
-func _on_player_interact(player):
-	# Case 1: Player is the active one (dismiss)
-	if active_player == player:
-		active_player.can_move = true
-		active_player = null
-		# Find PlayScript node (it's the CanvasLayer)
-		var ps = get_node("PlayScript")
-		if ps: ps.visible = false
-		return
-		
-	# Case 2: No active player, but this player is in zone (activate)
-	if active_player == null and player in players_in_zone:
-		active_player = player
-		player.can_move = false
-		var ps = get_node("PlayScript")
-		if ps: ps.visible = true
-
 func render_act(act_data):
 	var scenes = act_data.get("scene", [])
 	for scene_item in scenes:
