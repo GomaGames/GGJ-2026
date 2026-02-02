a2fdbfedba96634597980be3f83e1503f3ce8813
Jon Borgonia <jon.borgonia@gmail.com>
2026-02-01 16:12:27 -0800

    sfx: add success and failure

--- scripts/success_or_failure.gd
+++ scripts/success_or_failure.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+var audio_player: AudioStreamPlayer
+
+func _ready():
+    audio_player = AudioStreamPlayer.new()
+    add_child(audio_player)
 
 func success():
     show()
@@ -7,6 +12,26 @@ func success():
     var tween = create_tween()
     tween.tween_property(get_node("Success"), "modulate:a", 0.0, 0.6).from(1.0)
     tween.finished.connect(hide)
+    
+    # Play applause
+    var stream = preload("res://sfx/Applause.wav")
+    audio_player.stream = stream
+    
+    var duration = stream.get_length()
+    # Ensure we have at least 1 second to play
+    var max_start = max(0.0, duration - 1.0)
+    var start_pos = randf_range(0.0, max_start)
+    
+    audio_player.volume_db = -80.0
+    audio_player.play(start_pos)
+    
+    # Fade in and out
+    var audio_tween = create_tween()
+    audio_tween.tween_property(audio_player, "volume_db", 0.0, 0.2)
+    audio_tween.tween_interval(0.6)
+    audio_tween.tween_property(audio_player, "volume_db", -80.0, 0.2)
+    audio_tween.tween_callback(audio_player.stop)
+    audio_tween.tween_callback(func(): audio_player.volume_db = 0.0)
 
 
 func failure():
@@ -15,3 +40,9 @@ func failure():
     var tween = create_tween()
     tween.tween_property(get_node("Failure"), "modulate:a", 0.0, 0.6).from(1.0)
     tween.finished.connect(hide)
+    
+    # Play booing
+    var stream = preload("res://sfx/Booing.wav")
+    audio_player.stream = stream
+    audio_player.volume_db = 0.0
+    audio_player.play()
